d312fb94b5ded6e6faa74d3fcf200a5f579eaaa8
Homerocker <molodoy3561@gmail.com>
2026-06-11 00:11:34 +0300

    minor comments update

--- HomeCheck/Spells.lua
+++ HomeCheck/Spells.lua
@@ -1,16 +1,20 @@
 --[[
     cd                      - base cooldown in seconds
-    class                   - class name (capitalized english), only if spell is available for certain class
-    talentTab, talentIndex  - both must be specified if spell must be learned through talents
+    class                   - class name (CAPITALIZED english), only if spell is available for certain class
+    talentTab, talentIndex  - BOTH must be specified if spell must be learned through talents
     notarget                - true to hide spell target (for AoE, e. g. Divine Sacrifice applied on multiple raid members)
-    noself                  - true if spell caster should not be displayed as its target (workaround for MD and ToT auras applied on self)
+    noself                  - true if spell caster should not be displayed as its target
+                                (workaround for MD and ToT auras applied on self)
     tanksonly               - display cooldown for tanks only
     parent                  - share cooldown frame with specified spell id (e.g. MD cast and MD proc)
-    nocast                  - true if SPELL_AURA_APPLIED should be parsed instead of SPELL_CAST_SUCCESS (when spell is not casted by player directly; aura proc)
-    ignore                  - completely ignore spell by id, used for duplicate auras sharing same name for some spells (e.g. Tricks of the Trade cast + threat aura + damage buff aura)
-                              required as spell ids not present in this file will be looked up by spell names
+    nocast                  - true if spell DOES NOT trigger SPELL_CAST_SUCCESS and SPELL_AURA_APPLIED should be parsed instead
+                                (when spell is not casted by player directly; aura proc)
+    ignore                  - completely ignore spell by id, used for duplicate auras sharing same name for some spells
+                                (e.g. Tricks of the Trade cast + threat aura + damage buff aura)
+                                required as spell ids not present in this file will be looked up by spell names
 
-    notarget or noself can be applied to self-cast abilities and trinkets if you want to hide target name which is always equal to caster name
+    "notarget" or "noself" can be applied to self-cast abilities and trinkets if you want to hide target name which is always equal to caster name
+    (e. g. Shield Wall can only be casted on self and cannot be used on any raid member so we may not want to see its target)
 ]]
 
 HomeCheck.spells = {
